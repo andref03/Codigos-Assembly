6a317d7786c4d89711e3f547fdeb8453f68a0581
andref03 <andref.lopes@outlook.com>
2025-07-11 17:39:44 -0300

    separando a lógica em funções para 32 e para 64 bits (float e double respec.)

--- string_to_float.s
+++ string_to_float.s
@@ -38,36 +38,89 @@ _string_to_float:
     movl %eax, -8(%rbp)
 
     movl $10, %eax
-    cvtsi2sd %eax, %xmm1     # xmm1 = 10.0
-    movl $0, %eax
-    cvtsi2sd %eax, %xmm0     # xmm0 = 0.0
+    movl $0, %ebx
+
+    _is_float:
+        cmp $0, tipo(%rip)
+        jne _is_double
+        cvtsi2ss %eax, %xmm1     # xmm1 = 10.0
+        cvtsi2ss %ebx, %xmm0     # xmm0 = 0.0
+        jmp _if_ponto
+    _is_double:
+        cmp $1, tipo(%rip)
+        jne _fim_func_float
+        cvtsi2sd %eax, %xmm1     # xmm1 = 10.0
+        cvtsi2sd %ebx, %xmm0     # xmm0 = 0.0
 
     _if_ponto:
         cmp $'.', (%rdi)
-        jne _fim__func_float
+        jne _fim_func_float
         incq %rdi               # pula o ponto
 
+    
     _loop_fracionario:
         movzbq (%rdi), %rax     # %al = caractere atual
         cmpb $0, %al            # compara caractere atual com 0 (semelhante a '\0')
-        je _fim__loop_fracionario
+        je _fim_loop_fracionario
 
         call _char_para_digito  # retorno tá no %eax
         
         cmpl $-1, %eax          # se não for um dígito
-        je _fim__func_float
+        je _fim_func_float
 
-        cvtsi2sd %eax, %xmm2
-        
+        _if_float:
+            cmp $0, tipo(%rip)
+            jne _if_double
+            call _retorna_fracao_float
+            jmp _loop_fracionario
+        _if_double:
+            cmp $1, tipo(%rip)
+            jne _fim_func_float
+            call _retorna_fracao_double
+
+        incq %rdi
+        jmp _loop_fracionario
+
+    _fim_loop_fracionario:
 
 
-    _fim__loop_fracionario:
-
-
-
-    _fim__func_float:
+    _fim_func_float:
         ret
 
+
+_retorna_fracao_float:
+
+    pushq %rbp
+    movq %rsp, %rbp
+
+    cvtsi2ss %eax, %xmm2
+    divss %xmm1, %xmm2      # xmm2 = xmm2 / xmm1
+    addss %xmm2, %xmm0      # xmm0 = xmm0 + xmm2 (acumulando)
+
+    movl $10, %eax
+    cvtsi2ss %eax, %xmm3     # xmm3 = 10.0
+    mulss %xmm3, %xmm1       # xmm1 = xmm1 * 10.0
+
+    popq %rbp
+
+ret
+
+_retorna_fracao_double:
+
+    pushq %rbp
+    movq %rsp, %rbp
+
+    cvtsi2sd %eax, %xmm2
+    divsd %xmm1, %xmm2      # xmm2 = xmm2 / xmm1
+    addsd %xmm2, %xmm0      # xmm0 = xmm0 + xmm2 (acumulando)
+
+    movl $10, %eax
+    cvtsi2sd %eax, %xmm3     # xmm3 = 10.0
+    mulsd %xmm3, %xmm1       # xmm1 = xmm1 * 10.0
+
+    popq %rbp
+
+ret
 
 _string_to_int:
 
@@ -120,7 +173,6 @@ _string_to_int:
         addq $12, %rsp           # desaloca as 3 variáveis locais
         popq %rbp
 ret
-
 
 _char_para_digito:
 

9a52433318187a7aa4ae3c5ceec6c75fe2400b5a
andref03 <andref.lopes@outlook.com>
2025-08-09 18:45:02 -0300

    retirando excesso de comentários

--- string_to_int.s
+++ string_to_int.s
@@ -5,17 +5,17 @@ entrada:    .asciz "+99"
 .globl _start
 
 _start:
-
     pushq %rbp
     movq %rsp, %rbp
    
-    leaq entrada(%rip), %rdi    # ponteiro da string vai para %rdi
+    leaq entrada(%rip), %rdi
 
-    # %rdi é como se fosse o parâmetro pra função
+    # parâmetro %rdi: ponteiro da entrada
     call _string_to_int
 
     popq %rbp
-    movl %eax, %edi             # resultado está em %eax
+    # resultado em %eax
+    movl %eax, %edi
     movq $60, %rax
     syscall
 
@@ -24,18 +24,18 @@ _string_to_int:
     pushq %rbp
     movq %rsp, %rbp
     
-    subq $4, %rsp               # int resultado = -4(%rbp)
-    subq $4, %rsp               # int sinal = -8(%rbp)
-    subq $4, %rsp               # int digito = -12(%rbp)
+    subq $4, %rsp  # resultado = -4(%rbp)
+    subq $4, %rsp  # sinal = -8(%rbp)
+    subq $4, %rsp  # digito = -12(%rbp)
 
-    movl $0, -4(%rbp)           # resultado = 0
-    movl $1, -8(%rbp)           # sinal = 1
+    movl $0, -4(%rbp)
+    movl $1, -8(%rbp)
 
     _if:
-        movzbq (%rdi), %rax     # %al = caractere atual
-        cmpb $'-', %al          # compara caractere atual com '-'
+        movzbq (%rdi), %rax
+        cmpb $'-', %al
         jne _else
-        movl $-1, -8(%rbp)      # sinal = -1, pra multiplicar com resultado no final
+        movl $-1, -8(%rbp)
         incq %rdi
         jmp _loop_func
     _else:
@@ -44,8 +44,8 @@ _string_to_int:
         incq %rdi
 
     _loop_func:
-        movzbq (%rdi), %rax     # %al = caractere atual
-        cmpb $0, %al            # compara caractere atual com 0 (semelhatne a '\0')
+        movzbq (%rdi), %rax
+        cmpb $0, %al
         je _fim_func
 
         call _char_para_digito
@@ -54,20 +54,22 @@ _string_to_int:
         cmpl $-1, %eax
         je _fim_func
 
-        movl %eax, -12(%rbp)     # dígito = %eax
-        movl -4(%rbp), %eax      # %eax = resultado
-        imull $10, %eax, %eax    # %eax = %eax * 10
-        addl -12(%rbp), %eax     # %eax = %eax + dígito
-        movl %eax, -4(%rbp)      # resultado = %eax
+        # acumula valor do inteiro
+        movl %eax, -12(%rbp)
+        movl -4(%rbp), %eax
+        imull $10, %eax, %eax
+        addl -12(%rbp), %eax
+        movl %eax, -4(%rbp)
 
         incq %rdi
         jmp _loop_func
 
+    # aplica sinal
     _fim_func:
-        movl -8(%rbp), %edx      # %edx = sinal
-        movl -4(%rbp), %eax      # resultado
-        imull %edx, %eax         # resultado = resultado * sinal
-        addq $12, %rsp           # desaloca as 3 variáveis locais
+        movl -8(%rbp), %edx
+        movl -4(%rbp), %eax
+        imull %edx, %eax
+        addq $12, %rsp
         popq %rbp
         ret
 
@@ -75,19 +77,18 @@ _char_para_digito:
 
     pushq %rbp
     movq %rsp, %rbp
-    movzbq (%rdi), %rax         # %al = caractere atual
+    movzbq (%rdi), %rax
 
     _if_aux:
-        cmpb $'0', %al          # compara caractere atual com char '0'
+        cmpb $'0', %al
         jl _char_invalido
-        cmpb $'9', %al          # compara caractere atual com char '9'
+        cmpb $'9', %al
         jg _char_invalido
 
-        subb $'0', %al          # converte caractere para dígito inteiro de verdade
+        subb $'0', %al
         movzbl %al, %eax
         jmp _fim_func_aux
 
-    # se (%al < '0') ou (%al > '9'), não se trata de dígito
     _char_invalido:
         movl $-1, %eax
 

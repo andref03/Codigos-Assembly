4d09fefb6f04ac944eb35143cd59968c4470a879
andref03 <andref.lopes@outlook.com>
2025-08-10 00:22:47 -0300

    implementando string para short int através da comparação com string_to_int

--- string_to_short.s
+++ string_to_short.s
@@ -1,5 +1,5 @@
 .section .data
-entrada:    .asciz "115"
+entrada:    .asciz "135"
 
 .section .text
 .globl _start
@@ -11,85 +11,85 @@ _start:
     leaq entrada(%rip), %rdi    # ponteiro da string vai para %rdi
 
     # parâmetro %rdi: ponteiro da entrada
-    call _string_to_int
+    call _string_to_short
 
     popq %rbp
-    # resultado está em %eax
-    movl %eax, %edi
+    # resultado está em %ax
+    movzwl %ax, %edi
     movl $60, %eax
     syscall
 
 # ---------------------------------------------------------------------
 
-_string_to_int:
+_string_to_short:
     pushq %rbp
     movq %rsp, %rbp
     
-    subq $4, %rsp   # resultado = -4(%rbp)
-    subq $4, %rsp   # sinal = -8(%rbp)
-    subq $4, %rsp   # digito = -12(%rbp)
+    subq $2, %rsp   # resultado = -2(%rbp)
+    subq $2, %rsp   # sinal = -4(%rbp)
+    subq $2, %rsp   # digito = -6(%rbp)
 
-    movl $0, -4(%rbp)   # resultado = 0
-    movl $1, -8(%rbp)   # sinal = 1
+    movw $0, -2(%rbp)   # resultado = 0
+    movw $1, -4(%rbp)   # sinal = 1
 
     movzbl (%rdi), %eax   # %al: caractere atual
     cmpb $'-', %al
-    jne _else_str_to_int
-    movl $-1, -8(%rbp)    # sinal = -1
+    jne _else_str_to_short
+    movw $-1, -4(%rbp)    # sinal = -1
     addq $1, %rdi
-    jmp _loop_str_to_int
+    jmp _loop_str_to_short
 
-    _else_str_to_int:
+    _else_str_to_short:
         cmpb $'+', %al
-        jne _loop_str_to_int
+        jne _loop_str_to_short
         addq $1, %rdi
 
-    _loop_str_to_int:
+    _loop_str_to_short:
         movzbl (%rdi), %eax
         cmpb $0, %al
-        je _fim_str_to_int
+        je _fim_str_to_short
 
-        call _char_para_digito
+        call _char_para_digito_short
 
-        cmpl $-1, %eax
-        je _fim_str_to_int
+        cmpw $-1, %ax
+        je _fim_str_to_short
 
-        movl %eax, -12(%rbp)  # digito
-        movl -4(%rbp), %eax   # resultado
-        imull $10, %eax, %eax
-        addl -12(%rbp), %eax
-        movl %eax, -4(%rbp)
+        movw %ax, -6(%rbp)    # digito
+        movzwl -2(%rbp), %eax # resultado 
+        imulw $10, %ax, %ax
+        addw -6(%rbp), %ax
+        movw %ax, -2(%rbp)
 
         addq $1, %rdi
-        jmp _loop_str_to_int
+        jmp _loop_str_to_short
 
-    _fim_str_to_int:
-        movl -8(%rbp), %edx
-        movl -4(%rbp), %eax
-        imull %edx, %eax
-        addq $12, %rsp
+    _fim_str_to_short:
+        movw -4(%rbp), %dx
+        movw -2(%rbp), %ax
+        imulw %dx, %ax
+        addq $6, %rsp
         popq %rbp
         ret
 
 # ---------------------------------------------------------------------
 
-_char_para_digito:
+_char_para_digito_short:
     pushq %rbp
     movq %rsp, %rbp
     movzbl (%rdi), %eax
 
     cmpb $'0', %al
-    jl _char_invalido
+    jl _char_invalido_short
     cmpb $'9', %al
-    jg _char_invalido
+    jg _char_invalido_short
 
     subb $'0', %al
     movzbl %al, %eax
-    jmp _fim_char_para_digito
+    jmp _fim_char_para_digito_short
 
-    _char_invalido:
-        movl $-1, %eax
+    _char_invalido_short:
+        movw $-1, %ax
 
-    _fim_char_para_digito:
+    _fim_char_para_digito_short:
         popq %rbp
         ret

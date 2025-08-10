c02703754e18906e1bb2b5b8ac82a5952133ac84
andref03 <andref.lopes@outlook.com>
2025-08-09 23:55:06 -0300

    adequando long int para string, comparando-o com int para string

--- long_int_to_string.s
+++ long_int_to_string.s
@@ -1,5 +1,5 @@
 .section .data
-entrada:    .long 214
+entrada:    .quad  2872818712
 resultado:  .space 100
 quebra_linha:  .asciz "\n"
 
@@ -11,13 +11,13 @@ _start:
     movq %rsp, %rbp
 
     # parâmetros
-    movl entrada, %edi
+    movq entrada, %rdi
     leaq resultado, %rsi
 
-    call _int_to_string
+    call _long_int_to_string
 
     leaq resultado, %rdi
-    call _calcula_tamanho_str
+    call _calcula_tamanho_str_long_int
     movq %rax, %rdx # tamanho da string
     movq $1, %rax  
     movq $1, %rdi  
@@ -37,62 +37,62 @@ _start:
 
 # ---------------------------------------------------------------------
 
-_int_to_string:
+_long_int_to_string:
     pushq %rbp
     movq %rsp, %rbp
     pushq %rcx
     pushq %rbx
 
-    movl %edi, %eax # valor
+    movq %rdi, %rax # valor
     movq %rsi, %r8  # string
     movq $0, %rcx
-    movl $10, %ebx
+    movq $10, %rbx
 
     # número negativo
-    cmp $0, %eax
-    jge _positivo_int_to_str
+    cmp $0, %rax
+    jge _positivo_long_int_to_str
     movb $'-', (%r8)
     incq %r8
-    neg %eax
+    neg %rax
     
-    _positivo_int_to_str:
+    _positivo_long_int_to_str:
         # número zero
-        cmp $0, %eax
-        jne _loop_int_to_str
+        cmp $0, %rax
+        jne _loop_long_int_to_str
         movb $'0', (%r8)
         incq %r8
         movb $0, (%r8)
-        jmp _fim_int_to_str
+        jmp _fim_long_int_to_str
 
-    _loop_int_to_str:
-        movl $0, %edx
-        idivl %ebx          # divide eax por 10, edx = resto
+    _loop_long_int_to_str:
+        movq $0, %rdx
+        idivq %rbx          # divide rax por 10, rdx = resto
         addb $'0', %dl
         movb %dl, (%r8,%rcx,1)
         incq %rcx
-        cmp $0, %eax
-        jne _loop_int_to_str
+        cmp $0, %rax
+        jne _loop_long_int_to_str
 
         # inverte string
         movq %rcx, %r9
         decq %r9
         movq $0, %rax
 
-        _inverte_str:
+        _inverte_str_long_int:
             cmpq %rax, %r9
-            jle _fim_str
+            jle _fim_str_long_int
             movb (%r8,%rax,1), %dl
             movb (%r8,%r9,1), %cl
             movb %cl, (%r8,%rax,1)
             movb %dl, (%r8,%r9,1)
             incq %rax
             decq %r9
-            jmp _inverte_str
+            jmp _inverte_str_long_int
 
-    _fim_str:
+    _fim_str_long_int:
         movb $0, (%r8,%rcx,1)
 
-    _fim_int_to_str:
+    _fim_long_int_to_str:
         popq %rbx
         popq %rcx
         leave
@@ -100,17 +100,17 @@ _int_to_string:
 
 # ---------------------------------------------------------------------
 
-_calcula_tamanho_str:
+_calcula_tamanho_str_long_int:
     pushq %rbp
     movq %rsp, %rbp
     movq $0, %rax
     
-    _loop_tam_str:
+    _loop_tam_str_long_int:
         movb (%rdi,%rax,1), %cl
         cmpb $0, %cl
-        je _fim_loop_tam
+        je _fim_loop_tam_long_int
         inc %rax
-        jmp _loop_tam_str
-    _fim_loop_tam:
+        jmp _loop_tam_str_long_int
+    _fim_loop_tam_long_int:
         leave
         ret
